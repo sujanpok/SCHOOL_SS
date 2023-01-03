0f0173c098a6f2e5f1e80b869f04cf37b62f1189
sujan pokharel <pokharelisujna@gmail.com>
2023-01-03 18:44:29 +0900

    maven project

---
+++ src/test/java/school/edu/np/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package school.edu.np;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
